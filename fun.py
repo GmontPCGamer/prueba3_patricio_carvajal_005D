cf51e948fac2cb2712e2597e4e174bfd50273b39
Patricio <p.carvajal@duocuc.cl>
2024-07-03 11:00:00 -0400

    opc2_lista

--- fun.py
+++ fun.py
@@ -17,10 +17,34 @@ def menu():
 #RUT, nombre, dirección, comuna, detalle del pedido.
 def registrar_p():
     limpiar_p()
-    rut = int(input('Ingrese su rut sin puntos ni guion'))
-    nombre = input('Ingrese su nombre: ')
-    direccion = input('ingrese su dirección: ')
-    comuna = input('Ingrese su comuna')
+    while True:
+        try:
+            rut = int(input('Ingrese su rut sin puntos ni guion'))
+            if rut > 6:
+                break
+        except:
+            print("!ERROR DEBE SER UN NUMERO ENTERO")
+    while True:
+        nombre = input('Ingrese su nombre: ')
+        if len(nombre) > 2:
+            break
+        else:
+            print("Su nombre debe tener mas de 2 caracteres")
+            esperar_t
+    while True:
+        direccion = input('ingrese su dirección: ')
+        if len(direccion) > 5:
+            break
+        else:
+            print("Su dirección debe tener mas 5 caracteres")
+            esperar_t
+    while True:
+        comuna = input('Ingrese su comuna')
+        if len(comuna) > 3:
+            break
+        else:
+            print("Su comuna debe tener mas de 3 caracteres")
+            esperar_t
     print('Valor 5KG=12500$ Valor 15KG=22500$')
 
     t_venta15 = 0
@@ -32,10 +56,27 @@ def registrar_p():
         except:
             print("ERROR DEBE SER UN NUMERO ENTERO!!")
         if galon_g == 1:
+            while True:
+                try:
+                    cantidad = int(input('ingrese la cantidad a comprar'))
+                except:
+                    print("ERROR DEBE SER UN NUMERO ENTERO")
+                if cantidad > -1:
+                    break
+                else:
+                    print("Debe ser positivo o 0")
             cantidad = int(input('ingrese la cantidad a comprar'))
             t_venta5 = cantidad*precio_g1
         elif galon_g == 2:
-            cantidad = int(input('ingrese la cantidad a comprar'))
+            while True:
+                try:
+                    cantidad = int(input('ingrese la cantidad a comprar'))
+                except:
+                    print("ERROR DEBE SER UN NUMERO ENTERO")
+                if cantidad > -1:
+                    break
+                else:
+                    print("Debe ser positivo o 0")
             t_venta15 = cantidad*precio_g2
         salir = int(input('Desea agregar mas productos  1)si  2)no: '))
         total_c = t_venta15+t_venta5
@@ -45,13 +86,20 @@ def registrar_p():
             print('DETALLE')
             print("="*25)
             print("nombre  Dirección  Comuna  Gas.5KG Gas.15KG total")
+            puesto = 0
             for x in range(len(datos_clientes)):
-                print(datos_clientes[x])
+                puesto += puesto
+                print(datos_cliente)
             print("="*25)
             esperar_t()
             break
 def listar_todos_p():
-    pass
+    print("="*25)
+    print("nombre  Dirección  Comuna  Gas.5KG Gas.15KG total")
+    for x in range(len(datos_clientes)):
+        print(datos_clientes[x])
+    print("="*25)
+    esperar_t()
 def buscar_p_por_rut():
     pass
 def imprimir_hoja_ruta():
