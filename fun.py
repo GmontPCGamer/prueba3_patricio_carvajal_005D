621fdfabc15540f560997c7711ac7bb1b1faa2f7
Patricio <p.carvajal@duocuc.cl>
2024-07-03 09:50:44 -0400

    opc_1_sin:

--- fun.py
+++ fun.py
@@ -11,3 +11,33 @@ def menu():
     opc = int(input('\nIngrese opción: '))
 
     return opc
+
+#RUT, nombre, dirección, comuna, detalle del pedido.
+def registrar_p():
+    nombre = input('Ingrese su nombre: ')
+    direccion = input('ingrese su dirección: ')
+    comuna = input('Ingrese su comuna')
+
+    print('Valor 5KG=12500$ Valor 15KG=22500$')
+    while True:
+        try:
+            galon_g = int(input('Ingrese el galon de gas a solicitar, 1)5KG 2)15KG'))
+        except:
+            print("ERROR DEBE SER UN NUMERO ENTERO!!")
+        if galon_g == 1:
+            t_venta = galon_g*12500
+        elif galon_g == 2:
+            t_venta = galon_g*22500
+
+        salir = int(input('Desea agregar mas productos  1)si  2)no: '))
+        if salir == 2:
+            break
+def listar_todos_p():
+    pass
+def buscar_p_por_rut():
+    pass
+def imprimir_hoja_ruta():
+    pass
+def salir_p():
+    print('Hasta Pronto!')
+    exit()
--- menu_gas.py
+++ menu_gas.py
@@ -1,4 +1,15 @@
 from fun import *
 
 while True:
-    menu()
+    opc = menu()
+
+    if opc == 1:
+        registrar_p()
+    elif opc == 2:
+        listar_todos_p()
+    elif opc == 3:
+        buscar_p_por_rut()
+    elif opc == 4:
+        imprimir_hoja_ruta()
+    elif opc == 5:
+        salir_p()
