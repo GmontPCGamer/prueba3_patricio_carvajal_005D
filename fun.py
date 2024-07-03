eda288bdfec745f9dafad5e6ee65520853813541
Patricio <p.carvajal@duocuc.cl>
2024-07-03 10:15:15 -0400

    opc1_lista_sin_v

--- fun.py
+++ fun.py
@@ -1,5 +1,7 @@
 import csv, msvcrt, os
-
+datos_clientes = []
+precio_g1 = 12500
+precio_g2 = 22500
 def menu():
     print("MENU DE OPCIONES:")
     print("1) Registrar pedido")
@@ -14,22 +16,38 @@ def menu():
 
 #RUT, nombre, dirección, comuna, detalle del pedido.
 def registrar_p():
+    limpiar_p()
     nombre = input('Ingrese su nombre: ')
     direccion = input('ingrese su dirección: ')
     comuna = input('Ingrese su comuna')
-
     print('Valor 5KG=12500$ Valor 15KG=22500$')
+
     while True:
+        limpiar_p()
         try:
             galon_g = int(input('Ingrese el galon de gas a solicitar, 1)5KG 2)15KG'))
         except:
             print("ERROR DEBE SER UN NUMERO ENTERO!!")
+        t_venta15 = 0
+        t_venta5 = 0
         if galon_g == 1:
-            t_venta = galon_g*12500
+            cantidad = int(input('ingrese la cantidad a comprar'))
+            t_venta5 = cantidad*precio_g1
         elif galon_g == 2:
-            t_venta = galon_g*22500
+            cantidad = int(input('ingrese la cantidad a comprar'))
+            t_venta15 = cantidad*precio_g2
 
+        datos_cliente = nombre,direccion,comuna,galon_g,t_venta5,t_venta15
         salir = int(input('Desea agregar mas productos  1)si  2)no: '))
+        datos_clientes.append(datos_cliente)
+        print('DETALLE')
+        print("="*25)
+        print("nombre  Dirección  Comuna  Gas.5KG Gas.15KG total")
+        for x in range(len(datos_clientes)):
+            print(datos_clientes[x])
+        print("="*25)
+              
+        esperar_t()
         if salir == 2:
             break
 def listar_todos_p():
@@ -41,3 +59,12 @@ def imprimir_hoja_ruta():
 def salir_p():
     print('Hasta Pronto!')
     exit()
+
+
+################
+    
+def limpiar_p():
+    os.system('cls')
+def esperar_t():
+    print('presione tecla para continuar...')
+    msvcrt.getch()
