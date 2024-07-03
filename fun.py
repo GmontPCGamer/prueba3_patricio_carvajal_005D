18ee48a225565e7abae82712f6c41d007b29eaff
Patricio <p.carvajal@duocuc.cl>
2024-07-03 10:34:50 -0400

    opc1lista

--- fun.py
+++ fun.py
@@ -17,38 +17,38 @@ def menu():
 #RUT, nombre, dirección, comuna, detalle del pedido.
 def registrar_p():
     limpiar_p()
+    rut = int(input('Ingrese su rut sin puntos ni guion'))
     nombre = input('Ingrese su nombre: ')
     direccion = input('ingrese su dirección: ')
     comuna = input('Ingrese su comuna')
     print('Valor 5KG=12500$ Valor 15KG=22500$')
 
+    t_venta15 = 0
+    t_venta5 = 0
     while True:
         limpiar_p()
         try:
             galon_g = int(input('Ingrese el galon de gas a solicitar, 1)5KG 2)15KG'))
         except:
             print("ERROR DEBE SER UN NUMERO ENTERO!!")
-        t_venta15 = 0
-        t_venta5 = 0
         if galon_g == 1:
             cantidad = int(input('ingrese la cantidad a comprar'))
             t_venta5 = cantidad*precio_g1
         elif galon_g == 2:
             cantidad = int(input('ingrese la cantidad a comprar'))
             t_venta15 = cantidad*precio_g2
-
-        datos_cliente = nombre,direccion,comuna,galon_g,t_venta5,t_venta15
         salir = int(input('Desea agregar mas productos  1)si  2)no: '))
-        datos_clientes.append(datos_cliente)
-        print('DETALLE')
-        print("="*25)
-        print("nombre  Dirección  Comuna  Gas.5KG Gas.15KG total")
-        for x in range(len(datos_clientes)):
-            print(datos_clientes[x])
-        print("="*25)
-              
-        esperar_t()
+        total_c = t_venta15+t_venta5
+        datos_cliente = rut,nombre,direccion,comuna,galon_g,t_venta5,t_venta15, total_c
         if salir == 2:
+            datos_clientes.append(datos_cliente)
+            print('DETALLE')
+            print("="*25)
+            print("nombre  Dirección  Comuna  Gas.5KG Gas.15KG total")
+            for x in range(len(datos_clientes)):
+                print(datos_clientes[x])
+            print("="*25)
+            esperar_t()
             break
 def listar_todos_p():
     pass
